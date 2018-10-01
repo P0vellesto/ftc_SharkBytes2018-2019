13c710542f6109fbd2398076e58712a0309e6c7a
Exr0n <spotyie@gmail.com>
2018-10-01 16:38:43 -0700

    Started the Auto prog

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
@@ -0,0 +1,27 @@
+/*
+ * This started on 10/1/2018 by Albert H, following a swerve robotics tutorial:
+ * https://www.youtube.com/watch?annotation_id=annotation_2218718323&feature=iv&src_vid=sAZ2yJfJj4Q&v=OT_PGYIFBGE
+ * https://www.youtube.com/watch?v=kOapppDNlSA
+ */
+
+
+
+package org.firstinspires.ftc.teamcode;
+//https://www.youtube.com/watch?annotation_id=annotation_2218718323&feature=iv&src_vid=sAZ2yJfJj4Q&v=OT_PGYIFBGE
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="FirstAuto");
+
+public class auto extends LinearOpMode
+{
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        waitForStart();
+
+        
+    }
+
+}
