72b23df43b9c9f6ce42c036ab93fda29c348668a
P0vellesto <blueteagreensea@gmail.com>
2018-10-01 16:19:46 -0700

    Hi I did stuff to my code

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LIvi_Vuforia_CodeA.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LIvi_Vuforia_CodeA.java
@@ -1,4 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Vu
 public class LIvi_Vuforia_CodeA {
 }
