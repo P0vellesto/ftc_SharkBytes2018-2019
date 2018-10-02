94e695224609bf8a7384fee08104d7feb7c0004a
Nicholas Assaderaghi <nickassader@gmail.com>
2018-10-01 17:08:25 -0700

    My tele op code

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nic_Ben_Dhruv_TeleOP.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nic_Ben_Dhruv_TeleOP.java
@@ -1,9 +1,17 @@
+// IMPORTANT: Left and right joysticks are used to move the robot,
+// and a b are used to move the servo.
+// You hold a to lift the bucket,
+// and you hold be to make it go back.
+// The speed the servo turns when held will most
+// likely ber changed later after testing.
+// This code most likely needs to be debugged.
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-UM
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Tele")
 public class Nick_Tele_Op extends LinearOpMode
@@ -11,17 +19,34 @@ public class Nick_Tele_Op extends LinearOpMode
     private DcMotor motorLeft;
     private DcMotor motorRight;
 
+    private Servo bucket_plop;
+
     @Override
     public void runopmode() throws InterruptedException
     {
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
 
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        armServo = hardwareMap.servo.get("bucket_plop");
 
         waitForStart();
 
         while(opModeIsActive())
         {
+            motorLeft.setPower(-gamepad1.left_stick_y);
+            motorRight.setPower(-gamepad1.right_stick_y);
+
+            if(gamepad2.a)
+            {
+                armServo.setPosition(+ 0.01);
+            }
+
+            if(gamepad2.b)
+            {
+                armServo.setPosition(- 0.01);
+            }
 
             idle();
         }
