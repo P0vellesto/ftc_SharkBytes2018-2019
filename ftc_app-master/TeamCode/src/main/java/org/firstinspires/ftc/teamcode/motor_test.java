17d299b4d6fd3945468ada82305eaabc170c3231
Nicholas Assaderaghi <nickassader@gmail.com>
2018-10-29 16:21:25 -0700

    I made the functions for tele op and vuforia. They will be later modified for a 4 motor drive.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor_test.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor_test.java
@@ -98,11 +98,14 @@ public class motor_test extends OpMode{
         double right;
 
         // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+
         left = -gamepad1.left_stick_y;
         right = -gamepad1.right_stick_y;
 
-        robot.leftDrive.setPower(left);
-        robot.rightDrive.setPower(right);
+        power(left, right);
+        // Instead of using the two lines of code below, you can just use my method.
+        //robot.leftDrive.setPower(left);
+        //robot.rightDrive.setPower(right);
 
         // Use gamepad left & right Bumpers to open and close the claw
         if (gamepad1.right_bumper)
@@ -130,9 +133,40 @@ public class motor_test extends OpMode{
     }
 
     /*
+    The following code is for autonomous and teleop. For forward_backwards, put in a negative value to go backwards.
+    For the rest do a positive value.
+    */
+
+    //@Override
+    public void forward_backwards(double power)
+    {
+        robot.leftDrive.setPower(power);
+        robot.rightDrive.setPower(power);
+    }
+
+    //@Override
+    public void left(double power)
+    {
+        robot.leftDrive.setPower(power);
+        robot.rightDrive.setPower(-power);
+    }
+    //@Override
+    public void right(double power)
+    {
+        robot.leftDrive.setPower(-power);
+        robot.rightDrive.setPower(power);
+    }
+    //Override
+    public void power(double leftPower, double rightPower)
+    {
+        robot.leftDrive.setPower(leftPower);
+        robot.rightDrive.setPower(rightPower);
+    }
+    /*
      * Code to run ONCE after the driver hits STOP
      */
     @Override
     public void stop() {
     }
+
 }
